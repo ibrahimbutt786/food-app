9743c873c56c083fe7dda1e05b0aed39ee409eda
ibrahimbutt786 <145439435+ibrahimbutt786@users.noreply.github.com>
2024-02-11 07:49:11 +0000

    done

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1 className='border text-2xl text-center my-10 font-' >Home Page</h1>
+    </div>
+  )
+}
+
+export default page
